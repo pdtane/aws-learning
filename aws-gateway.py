572be65a150eb0e3b4d89aff6416d2b81b893974
Peter Tane <peter.tane@gmail.comm>
2019-05-01 09:15:08 -0600

    Add use of resources

--- aws-gateway.py
+++ aws-gateway.py
@@ -3,14 +3,15 @@ import pprint
 
 apiGatewayClient = boto3.client('apigateway')
 ec2Client = boto3.client('ec2')
+ec2Resource = boto3.resource('ec2')
 
-# response = apiGatewayClient.get_rest_apis()
-# print('Existing Rest APIs:')
-# for api in response['items']:
-#     print(api['name'])
-#     r = apiGatewayClient.get_deployments(restApiId=api['id'])
-#     for deployment in r['items']:
-#         print(deployment)
+response = apiGatewayClient.get_rest_apis()
+print('Existing Rest APIs:')
+for api in response['items']:
+    print(api['name'])
+    r = apiGatewayClient.get_deployments(restApiId=api['id'])
+    for deployment in r['items']:
+        print(deployment)
 
 instances = []
 response = ec2Client.describe_instances()
@@ -21,3 +22,8 @@ for reservation in response['Reservations']:
 
 for i in instances:
     pprint.pprint(i)
+
+myInstances = ec2Resource.instances.all()
+for instance in myInstances:
+    print(instance)
+
