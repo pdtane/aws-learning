f7ad54db1759d069b0f9c9bd451a5534a80fc452
Peter Tane <peter.tane@gmail.comm>
2019-05-01 09:32:40 -0600

    uncommented other api calls

--- aws-sdk.js
+++ aws-sdk.js
@@ -1,21 +1,23 @@
 const AWS = require('aws-sdk');
-AWS.config.update({region:'us-west-2'});
+AWS.config.update({
+    region:'us-west-2',
+    logger: console
+});
 
 const apiGateway = new AWS.APIGateway();
 const cloudformation = new AWS.CloudFormation();
 const ec2 = new AWS.EC2();
-
 
 (async () => {
     let restApis = await apiGateway.getRestApis({}).promise();
     restApis.items.forEach(async (api) => {
         const deployments = 
             await apiGateway.getDeployments( { restApiId: api.id }).promise();
-        /* console.log(deployments); */
+        console.log(deployments);
     });
 
     let cfStacks = await cloudformation.describeStacks().promise();
-    /* cfStacks.Stacks.forEach(s => console.log(s)); */
+    cfStacks.Stacks.forEach(s => console.log(s));
 
     const ec2Instances = await ec2.describeInstances().promise();
     const instances = ec2Instances.Reservations.reduce((acc, e) => [...acc, ...e.Instances], []);
